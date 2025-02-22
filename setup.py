79354843df7a6d8580688c791767abf1d52b66da
Talal Alqahs <153442159+TFQ0@users.noreply.github.com>
2025-02-22 21:05:51 +0300

    Now version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="tfq0tool",            
-    version="0.2.1",            
+    version="0.2.2",            
     author="Talal",
     description="is a command-line utility for extracting text from various file formats, including text files, PDFs, Word documents, spreadsheets, and code files in popular programming languages.",
     long_description=long_description,
