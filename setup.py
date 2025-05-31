20102fa61162ef255e0d99c9e4cda4cd5347bdd6
Talal Alqahs <153442159+TFQ0@users.noreply.github.com>
2025-05-31 06:23:01 +0300

    v2
    v2

--- setup.py
+++ setup.py
@@ -11,7 +11,11 @@ setup(
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     version="1.2.5",            
+=======
+    version="0.3.0",            
+>>>>>>> Stashed changes
 =======
     version="0.3.0",            
 >>>>>>> Stashed changes
