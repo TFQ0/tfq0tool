65f25cc89e37f361c4dd64f37f39d82345baa449
Talal Alqahs <153442159+TFQ0@users.noreply.github.com>
2025-05-31 07:14:43 +0300

    v
    v

--- tfq0tool/__init__.py
+++ tfq0tool/__init__.py
@@ -7,7 +7,7 @@ and advanced text processing features.
 """
 
 from .tfq0tool import main
-__version__ = "0.3.0"
+__version__ = "2.0.0"
 __author__ = "Talal"
 __description__ = "is a command-line utility for extracting text from various file formats, including text files, PDFs, Word documents, spreadsheets, and code files in popular programming languages."
 __all__ = ["TextExtractor", "FileProcessor", "utils"]
